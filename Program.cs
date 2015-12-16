879f094f22f4e56ec11a12fa33d96478edf0221c
Udham1 <udhams@360logica.com>
2015-12-16 20:16:44 +0530

    Initial check-in of module sdafsjkdsjjdsfjsd

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+
+namespace sdafsjkdsjjdsfjsd
+{
+	using System;
+
+	public static class Program
+	{
+		public static void Main()
+		{
+			Action a = M1;
+			a += M2;
+			WriteAction(a);
+		}
+
+
+		public static void WriteAction(Action action)
+		{
+			Console.WriteLine(action.Method.Name);
+		}
+
+		public static void M1()
+		{
+
+		}
+
+		public static void M2()
+		{
+
+		}
+	}
+}
